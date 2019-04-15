70ee408c42728a918ca22f0fa62590ea24fe22b5
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-04-15 17:17:04 +0530

    Modified homepage for user

--- Sithivili/src/pages/home/home.ts
+++ Sithivili/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Alert,AlertController, NavController } from 'ionic-angular';
+import { ChatPage } from '../chat/chat';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  username:string='';
+
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+    
 
   }
+
+  alert(title:string,message:string){
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  
+  loginUser(){
+    if(/^[a-zA-Z0-9]+$/.test(this.username)){
+      //Valid
+      this.navCtrl.push(ChatPage,{
+        username:this.username
+      });
+
+    }else{
+      this.alert('Error','Invalid Username');
+    }
+    
+  }
+  
 
 }
