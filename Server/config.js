4bdbbd2590b383f62978f97ce90890221cf863c2
Thathsara Nandun <w1697812@my.westminster.ac.uk>
2019-07-05 16:15:29 +0530

    Only client files

--- Server/config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-    host    : 'sql12.freemysqlhosting.net',
-    user    : 'sql12295341',
-    password: 'Asv3F9562X',
-    database: 'sql12295341'
-  };
-   
-  module.exports = config;
