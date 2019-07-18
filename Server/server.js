f552ac11a23ae04e9b84b55e7d5d33b2b43bd926
Thathsara Nandun <46743657+thathsaranandun@users.noreply.github.com>
2019-07-18 09:19:49 +0530

    Updated cors

--- Server/server.js
+++ Server/server.js
@@ -7,8 +7,33 @@ app.use(function(req, res, next) {
   next();
 });
 
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100'
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+}
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
+
+app.get('/', cors(corsOptions), (req, res, next) => {
+  res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
+})
   //GET user request
-  app.route('/api/users/:id').get((req, res) => {
+  app.route('/api/users/:id',cors(corsOptions).get((req, res) => {
     const requestedID = req.params['id']
     let mysql = require('mysql');
     let config = require('./config.js');
